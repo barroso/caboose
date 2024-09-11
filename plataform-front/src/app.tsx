4664d3507d951ab058fd767668e573ae59574088
barroso <franciscobarrosoflf@gmail.com>
2024-09-10 21:10:38 -0300

    refactor: remove company code from landing-page and add Trusted component

--- plataform-front/src/app.tsx
+++ plataform-front/src/app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/not-found";
-import Board from "./pages/board";
+import LandingPage from "./pages/landing-page";
 import About from "./pages/about";
 import Header from "./components/header";
 import Brand from "./pages/brand";
@@ -13,7 +13,7 @@ const App = () => {
 			<Header />
 			<Suspense fallback={<div className="container">Loading...</div>}>
 				<Routes>
-					<Route path="/" element={<Board />} />
+					<Route path="/" element={<LandingPage />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/brand" element={<Brand />} />
 					<Route path="*" element={<NotFound />} />
